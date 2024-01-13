270312266c8ed208afe90a9a6e10a4ed3df8ce2f
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2024-01-13 13:55:20 +0330

    Update Final

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -3,12 +3,16 @@ import { AiFillHeart } from "react-icons/ai";
 
 import styles from "./BookCard.module.css";
 
-function BookCard({ data: { title, author, image, language, pages } }) {
+function BookCard({ data, handleLikedList }) {
+  const { title, author, image, language, pages } = data;
+
   const [like, setLike] = useState(false);
 
   const likeHandler = () => {
+    handleLikedList(data, like);
     setLike((like) => !like);
   };
+
   return (
     <div className={styles.card}>
       <img src={image} alt={title} />
